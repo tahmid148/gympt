91e7a298ab72dda50d238aa585ce72266a7cf1f8
tahmid <tahmidrahman2812@outlook.com>
2025-06-29 22:35:53 +0100

    feat: formatting sorted out

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,35 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
+const {defineConfig} = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+const reactNativePlugin = require('eslint-plugin-react-native');
+const importPlugin = require('eslint-plugin-import');
 
 module.exports = defineConfig([
-  expoConfig,
+  ...expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist', '.expo', 'node_modules'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+      'react-native': reactNativePlugin,
+      import: importPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react-native/no-inline-styles': 'off',
+    },
   },
 ]);
