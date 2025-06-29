b59b84420010c2aabdbacdbe1cd45fbafa453426
tahmid <tahmidrahman2812@outlook.com>
2025-06-29 23:02:20 +0100

    feat: added option for dark mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
+import {DarkTheme, DefaultTheme} from '@/constants/Themes';
+import {ThemeProvider} from '@react-navigation/native';
 import {Stack} from 'expo-router';
+import {useColorScheme} from 'react-native';
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack />
+    </ThemeProvider>
+  );
 }
